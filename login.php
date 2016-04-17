c273898c6bfbe90ea122226f357d2fd9ae722c25
stsnqf <stsnqf@mail.missouri.edu>
2016-04-17 18:32:48 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,6 @@
 <?php
 	session_start();
-	if (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) { // if request is not secure, redirect to secure url
-           $url = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-           header('Location: ' . $url);
-	}
+	
 ?>
 <html>
         <head>
