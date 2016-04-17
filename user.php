723603d28bcc06377d1f22255b180431b8dc7200
stsnqf <stsnqf@mail.missouri.edu>
2016-04-17 18:33:41 -0500

    Update user.php

--- user.php
+++ user.php
@@ -4,12 +4,7 @@
 		echo"You are not logged in, redirecting";
 		header('Location: http://pikachu-swacy.centralus.cloudapp.azure.com/login.php');
 	}
-	
-	if (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) { // if request is not secure, redirect to secure url
-           $url = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-           header('Location: ' . $url);
-	}
-	
+
 ?>
 <html>
 	<head>
