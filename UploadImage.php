c7db03f2a6bcb4aedd98407bcbad81661dbaa717
stsnqf <stsnqf@mail.missouri.edu>
2016-04-17 22:20:25 -0500

    Update UploadImage.php

--- UploadImage.php
+++ UploadImage.php
@@ -5,7 +5,7 @@ $UploadedFileName=$_FILES['UploadImage']['name'];
 if($UploadedFileName!='')
 {
                 $upload_directory = "MyUploadImages/"; //This is the folder which you created just now
-        $TargetPath=time().$UploadedFileName;
+                $TargetPath=time().$UploadedFileName;
                 if(move_uploaded_file($_FILES['files']['tmp_name'], $upload_directory.$TargetPath))
                 {    
                     $QueryInsertFile="INSERT INTO user SET picture='$TargetPath'"; 
@@ -13,6 +13,7 @@ if($UploadedFileName!='')
                     $link = mysqli_connect("pikachu-swacy.centralus.cloudapp.azure.com", "general", "Thisistheultimatepassword1337", "mydb") or die ("Connection Error " . mysqli_error($link));
                     if ($stmt = mysqli_prepare($link, $QueryInsertFile)) {
                         mysqli_stmt_bind_param($stmt, "s", $QueryInsertFile) or die ("photobind");
+                        header('Location: /register.php');
                     }
                 
                 }
